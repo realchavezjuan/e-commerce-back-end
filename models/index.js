12c7650677cad8c35735fad29377a12a78a23969
Juan Chavez <juan@Juans-Air.home>
2022-02-23 16:59:36 -0800

    built models and associations

--- models/index.js
+++ models/index.js
@@ -4,14 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_id'
+})
 module.exports = {
   Product,
   Category,
