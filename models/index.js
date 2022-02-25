d4ea4ced25f562f675546716afbedfc2d78efee4
Juan Chavez <juan@Juans-Air.home>
2022-02-24 17:24:44 -0800

    fixed assossiation between tag and product, now i am able to seed the database

--- models/index.js
+++ models/index.js
@@ -14,12 +14,13 @@ Product.belongsTo(Category, {
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'tag_id'
+  through: 'ProductTag'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id'
+  through: 'ProductTag'
 })
+
 module.exports = {
   Product,
   Category,
